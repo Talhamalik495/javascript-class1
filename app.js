fc3cd5630bbed44663f1908e702bb84aedb44bab
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2024-02-29 13:04:29 +0500

    Updated

---
+++ app.js
@@ -0,0 +1,7 @@
+let firsts = +prompt("Enter vlue1")
+let seconds = +prompt("Enter value2")
+
+document.write("Addition", firsts + seconds, "<br>");
+document.write("Subtraction", firsts - seconds, "<br>");
+document.write("Multiplication", firsts * seconds, "<br>");
+document.write("Divison", "", firsts / seconds, "<br>")
